7828199c7da5aaf177573559ae32af85acd4d704
Your Name <moseoduor86@gmail.com>
2021-08-10 22:44:07 +0300

    first commit

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import com.example.exception.UserNotFoundException;
+import com.example.model.Employee;
+import com.example.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return  employeeRepo.save(employee);
+    }
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(()->new UserNotFoundException("user not found"));
+    }
+}
